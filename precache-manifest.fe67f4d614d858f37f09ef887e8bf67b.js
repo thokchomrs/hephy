423ffa07ded04f1316c20c050fa94e12d21cd8b2
thokchom <thokchomrs@gmail.com>
2020-04-01 15:59:03 +0530

    Updates

---
+++ precache-manifest.fe67f4d614d858f37f09ef887e8bf67b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec61ebe3160c954e4088f8e36d4fb75f",
+    "url": "/hephy/index.html"
+  },
+  {
+    "revision": "9a581a51577e5787498e",
+    "url": "/hephy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "df5a5c85d391cac4bdec",
+    "url": "/hephy/static/css/main.85e75f7f.chunk.css"
+  },
+  {
+    "revision": "9a581a51577e5787498e",
+    "url": "/hephy/static/js/2.b3b651f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hephy/static/js/2.b3b651f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5a5c85d391cac4bdec",
+    "url": "/hephy/static/js/main.4ffe4520.chunk.js"
+  },
+  {
+    "revision": "d11c86f6d6793f37ca75",
+    "url": "/hephy/static/js/runtime-main.3721a5d1.js"
+  },
+  {
+    "revision": "93e9c90bb923aee04207c51ea310b24c",
+    "url": "/hephy/static/media/G1.93e9c90b.png"
+  },
+  {
+    "revision": "30b3550b1bbcfbd326bfe9f004dcbbb2",
+    "url": "/hephy/static/media/G1s.30b3550b.jpg"
+  },
+  {
+    "revision": "0b399d57f2fcc8a2a3ae991a14a4d008",
+    "url": "/hephy/static/media/G2.0b399d57.png"
+  },
+  {
+    "revision": "60aa7458c0d74d9289de6e7b54c38f6b",
+    "url": "/hephy/static/media/G2s.60aa7458.png"
+  },
+  {
+    "revision": "6604bdff7d0f8fc57e14b23b5d58af16",
+    "url": "/hephy/static/media/logo.6604bdff.png"
+  },
+  {
+    "revision": "9d7a25eb1773cca8d494acad679a2b15",
+    "url": "/hephy/static/media/site-bg.9d7a25eb.jpg"
+  },
+  {
+    "revision": "9c8e1ab8991a76a7550595e05e24012a",
+    "url": "/hephy/static/media/slide_1.9c8e1ab8.jpg"
+  },
+  {
+    "revision": "8c3cd0abb3365ec2f08997975d1dbe18",
+    "url": "/hephy/static/media/slide_2.8c3cd0ab.jpg"
+  },
+  {
+    "revision": "30b77f1c9f3822f576e897214b1135c4",
+    "url": "/hephy/static/media/slide_3.30b77f1c.jpg"
+  }
+]);
